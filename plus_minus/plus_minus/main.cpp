cc0580a76d5821e2a128a9a1fa744c6b03c2716d
Pupsik <ThePupsick@yandex.ru>
2016-02-25 23:24:50 +0300

    My Bullshit code

---
+++ plus_minus/plus_minus/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  plus_minus
+//
+//  Created by Виталя on 25.02.16.
+//  Copyright © 2016 Dreams of Pines. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void inputBigNumber(int base, int bigNumber[], int baseMax)
+{
+    string value = "";
+    cin >> value;
+    
+
+}
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
