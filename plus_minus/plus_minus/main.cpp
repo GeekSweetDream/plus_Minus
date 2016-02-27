a091328aca23399f0946418c44cc7a75a90c958f
Pupsik <ThePupsick@yandex.ru>
2016-02-27 14:37:46 +0300

    BeginCoding
    First step for exp

--- plus_minus/plus_minus/main.cpp
+++ plus_minus/plus_minus/main.cpp
@@ -11,15 +11,51 @@
 
 using namespace std;
 
-void inputBigNumber(int base, int bigNumber[], int baseMax)
+
+string inputBigNumber();
+string operations(char sign, string firstNumb, string secondNumb);
+int giveNumbLong(string firstNumb, string secondNumb);
+
+int main(int argc, const char * argv[])
+{
+    string test = "0123";
+    cout << "12"+test << endl;
+    cout << 'a'-'0'<< endl;
+    return 0;
+}
+
+string inputBigNumber()
 {
     string value = "";
     cin >> value;
-    
-
+    return value;
 }
 
-int main(int argc, const char * argv[]) {
-
+string operations(char sign, string firstNumb, string secondNumb)
+{
+    const string symbolArray = "0123456789ABCDEFG"; //Дописать;
+    int maxLength = (int) firstNumb.length();
+    if (giveNumbLong(firstNumb, secondNumb))
+    {
+        maxLength = (int) secondNumb.length();
+    }
+    for (int i = maxLength - 1; i >= 0; i--)
+    {
+       // if ()
+    }
     return 0;
 }
+
+int giveNumbLong(string firstNumb, string secondNumb)
+{
+    int winner = 0;
+    if (firstNumb.length() < secondNumb.length())
+    {
+        winner = 1;
+    }
+    return winner;
+}
+
+
+
+
